985381685d25198aada13deaeac72fa1f11c1231
harshtripathi24 <harshtripathistar@gmail.com>
2021-09-27 15:50:30 +0530

    Added pges

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Nav';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        Home
-      </Route>
-      <Route exact path="/start">
-        Start
-      </Route>
-      <Route path="*">This is 404 Page</Route>
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route path="*">This is 404 Page</Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/pages/Starred.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Starred = () => {
+  return <div>This is Start</div>;
+};
+
+export default Starred;
