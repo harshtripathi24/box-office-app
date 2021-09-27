35cf1a6ac91dc4645616de5b954f302ded1abd7a
harshtripathi24 <harshtripathistar@gmail.com>
2021-09-27 18:54:12 +0530

    Added Page Mangement and title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navs from './components/Nav';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
 function App() {
   return (
-    <div>
-      <Navs />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-        <Route path="*">This is 404 Page</Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route path="*">This is 404 Page</Route>
+    </Switch>
   );
 }
 
--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Starred = () => {
-  return <div>This is Start</div>;
+  return <div> This is Starred</div>;
 };
 
 export default Starred;
