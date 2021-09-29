a9c3f99ff53d85605be7bc323bd8201246cbd0e9
harshtripathi24 <harshtripathistar@gmail.com>
2021-09-29 17:31:36 +0530

    Added useReducer instead of useState in show

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,15 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router';
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, isError: null, show: action.show };
+    }
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, isError: action.message };
+    }
+
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  isError: null,
+};
 
 const Show = () => {
   const { id } = useParams();
-  const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(null);
+
+  const [{ show, isLoading, isError }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  // const [show, setShow] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [isError, setIsError] = useState(null);
 
   useEffect(() => {
     let isMounted = true;
-    setIsLoading(true);
     const fetchData = async () => {
       try {
         const res = await fetch(
@@ -18,14 +42,12 @@ const Show = () => {
         const data = await res.json();
         setTimeout(() => {
           if (isMounted) {
-            setShow(data);
-            setIsLoading(false);
+            dispatch({ type: 'FETCH_SUCCESS', show: data });
           }
         }, 2000);
       } catch (error) {
         if (isMounted) {
-          setIsError(error);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_FAILED', message: error.message });
         }
       }
 
@@ -38,14 +60,13 @@ const Show = () => {
   }, [id]);
 
   console.log(show);
-
   if (isLoading) {
     return <div>Is Loading</div>;
   }
   if (isError) {
-    return <div>fdsf</div>;
+    return <div>Error occured {isError}</div>;
   }
-  return <div>Error occured {isError}</div>;
+  return <div>Data Loaded</div>;
 };
 
 export default Show;
