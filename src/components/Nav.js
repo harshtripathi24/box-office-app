53c649aaa7c9cac4f60b6adf230784c6000649a1
harshtripathi24 <harshtripathistar@gmail.com>
2021-09-30 19:14:20 +0530

    added stye in component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
+import { LinkStyled, NavList } from './Nav.styled';
 
 const LINKS = [
   { to: '/', text: 'Home' },
@@ -7,15 +8,21 @@ const LINKS = [
 ];
 
 const Navs = () => {
+  const location = useLocation();
   return (
     <div>
-      <ul>
+      <NavList>
         {LINKS.map(item => (
           <li key={item.to}>
-            <Link to={item.to}>{item.text}</Link>
+            <LinkStyled
+              className={item.to === location.pathname ? 'active' : ''}
+              to={item.to}
+            >
+              {item.text}
+            </LinkStyled>
           </li>
         ))}
-      </ul>
+      </NavList>
     </div>
   );
 };
